b0f928341ed1d8ecf1632ee14f3e095af54ad60a
Minh Trần <wisekingtran@gmail.com>
2024-06-27 17:20:10 +0700

    test support

--- common-technique-test-support/src/main/java/vn/ifa/study/CommonTechniqueTestSupportApplication.java
+++ common-technique-test-support/src/main/java/vn/ifa/study/CommonTechniqueTestSupportApplication.java
@@ -1,13 +1,46 @@
 package vn.ifa.study;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import vn.ifa.study.oo.model.StoredObject;
+import vn.ifa.study.oo.service.OSClient;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 @SpringBootApplication
 public class CommonTechniqueTestSupportApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommonTechniqueTestSupportApplication.class, args);
-	}
+    @Autowired
+    private OSClient minio;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonTechniqueTestSupportApplication.class, args);
+    }
+
+
+    @PostConstruct
+    void init() throws IOException {
+
+        Path filePath = Path.of("./data", "kofile/019/00473.tif");
+        File file = filePath.toFile();
+        if (!file.exists()) {
+            file.getParentFile()
+                .mkdirs();
+            file.createNewFile();
+        }
+        OutputStream os = Files.newOutputStream(filePath, StandardOpenOption.WRITE);
+        minio.getObject(StoredObject.builder()
+                                    .bucket("intodigital")
+                                    .key("kofile/019/00473.tif")
+                                    .build(), os);
+
+    }
 
 }
