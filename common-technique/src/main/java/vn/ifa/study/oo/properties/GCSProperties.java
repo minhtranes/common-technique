826921496b19359afbb4e82189f5f183137936d6
Minh Trần <wisekingtran@gmail.com>
2024-06-28 09:18:54 +0700

    Update

--- common-technique/src/main/java/vn/ifa/study/oo/properties/GCSProperties.java
+++ common-technique/src/main/java/vn/ifa/study/oo/properties/GCSProperties.java
@@ -1,10 +1,12 @@
 package vn.ifa.study.oo.properties;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Builder
 public class GCSProperties extends AbstractObjectStorageProperties{
     private String projectId;
 }
